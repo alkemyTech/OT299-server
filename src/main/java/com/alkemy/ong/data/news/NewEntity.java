f854ab5560ea05d886a126efc873f101e62e3e76
etchichuryg <ginettetchichury@gmail.com>
2022-09-14 21:38:17 +0200

    Entity and Repository New

---
+++ src/main/java/com/alkemy/ong/data/news/NewEntity.java
@@ -0,0 +1,43 @@
+package com.alkemy.ong.data.news;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Table (name = "news")
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class NewEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String content;
+
+    @Column(nullable = false)
+    private String image;
+
+    private Long categoryid;
+
+    @Column(name = "created_at")
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date createdAt;
+
+    @Column(name = "updated_at")
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date updatedAt;
+
+    private Boolean deleted = false;
+
+}
