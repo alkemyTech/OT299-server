8866c81fd87f84fd41e4bf80921764755693fe68
Linda <lindacristal.parrasanhueza@gmail.com>
2022-09-14 17:49:21 -0300

    Update interface for repository, CrudRepository -> JpaRepository

--- src/main/java/com/alkemy/ong/data/user/UserRepository.java
+++ src/main/java/com/alkemy/ong/data/user/UserRepository.java
@@ -1,11 +1,11 @@
 package com.alkemy.ong.data.user;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
-public interface UserRepository extends CrudRepository {
+public interface UserRepository extends JpaRepository<UserEntity, Long> {
     List<UserEntity> findAll();
 }
