5f39a6962863f90d2e8143f4b068964a5c8183b2
Angela Monsalve <angelapmonsalve@gmail.com>
2022-09-14 22:22:29 -0500

    Added script to create comments table in schema.sql

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -6,3 +6,14 @@ CREATE TABLE IF NOT EXISTS SAMPLE(
   deleted      BIT         DEFAULT 0,
   PRIMARY KEY (id)
 );
+CREATE TABLE IF NOT EXISTS COMMENTS(
+  id           BIGINT(50)  NOT NULL AUTO_INCREMENT,
+  body  TEXT DEFAULT NULL,
+  user_id BIGINT(50) NOT NULL,
+  news_id BIGINT(50) NOT NULL,
+  created_at    TIMESTAMP   DEFAULT NOW(),
+  updated_at    TIMESTAMP   DEFAULT NOW(),
+  deleted      BIT         DEFAULT 0,
+  PRIMARY KEY (id)
+
+);
