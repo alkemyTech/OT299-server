48bf816cd67bd852b1be639b22563fdb8e9b89dc
NicolasGastonVarela <101136113+NicolasGastonVarela@users.noreply.github.com>
2022-09-15 17:21:06 -0300

    ContactsEntity created, ContactsRepository created, schema updated

---
+++ src/main/java/com/alkemy/ong/data/repositories/ContactsRepository.java
@@ -0,0 +1,9 @@
+package com.alkemy.ong.data.repositories;
+
+import com.alkemy.ong.data.entities.ContactsEntity;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ContactsRepository extends CrudRepository<ContactsEntity, Long> {
+}
