4ee4592a00596a9caa5566e2049be3c8ea1f897a
angelamonsalvep <54193886+angelamonsalvep@users.noreply.github.com>
2022-09-15 14:56:02 -0500

    creation of the model, repository and script for the Organizations
    * creation of the model, repository and script for the Organizations entity.
    
    * change directory structure in package Data(
    grouping by entities and repositories)
    
    * model is created for the Comments entity.
    
    * Revert "model is created for the Comments entity."
    
    This reverts commit d843e6422d48dc059d85479d28cec121be4487fa.
    
    * Resolved pullrequest fixes.

---
+++ src/main/java/com/alkemy/ong/data/entities/OrganizationEntity.java
@@ -0,0 +1,59 @@
+package com.alkemy.ong.data.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.UpdateTimestamp;
+import org.hibernate.annotations.Where;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.Date;
+
+@Entity
+@Table(name = "organizations")
+@SQLDelete(sql = "UPDATE organizations SET deleted=true WHERE id=?")
+@Where(clause = "deleted = false")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class OrganizationEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String image;
+
+    private String address;
+
+    private int phone;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column(name = "welcome_text", nullable = false)
+    private String welcomeText;
+
+    @Column(name = "about_us_text")
+    private String aboutUsText;
+
+    @Column(name = "update_at")
+    @UpdateTimestamp()
+    private LocalDateTime updatedAt;
+
+    @Column(name = "created_at")
+    @CreationTimestamp()
+    private LocalDateTime createdAt;
+
+    private boolean deleted = false;
+
+}
