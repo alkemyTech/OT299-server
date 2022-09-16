25876324eeae77a61269856fb5de52ee884507d3
NicolasGastonVarela <101136113+NicolasGastonVarela@users.noreply.github.com>
2022-09-16 09:58:53 -0300

    Requested changes resolved Changes updated in schema

--- src/main/java/com/alkemy/ong/data/repositories/ContactsRepository.java
+++ src/main/java/com/alkemy/ong/data/repositories/ContactsRepository.java
@@ -4,6 +4,9 @@ import com.alkemy.ong.data.entities.ContactsEntity;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ContactsRepository extends CrudRepository<ContactsEntity, Long> {
+    List<ContactsEntity> findAll();
 }
