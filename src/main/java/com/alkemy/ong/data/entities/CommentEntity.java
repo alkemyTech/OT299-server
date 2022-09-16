54f290ec8a96f909b705f75ddfcf84501490de29
angelamonsalvep <54193886+angelamonsalvep@users.noreply.github.com>
2022-09-16 14:54:05 -0500

    creation of the model, repository and script for the Comments entity. (#8)
    * model is created for the Comments entity.
    
    * Comments repository is created.
    
    * Added script to create comments table in schema.sql
    
    * Correction of fields in the Comment entity.
    
    * Resolved pullrequest fixes.

---
+++ src/main/java/com/alkemy/ong/data/entities/CommentEntity.java
@@ -0,0 +1,48 @@
+package com.alkemy.ong.data.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.UpdateTimestamp;
+import org.hibernate.annotations.Where;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.Date;
+
+@Entity
+@Table(name = "comments")
+@SQLDelete(sql = "UPDATE comments SET deleted=true WHERE id=?")
+@Where(clause = "deleted = false")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class CommentEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "user_id")
+    private long userId;
+
+    private String body;
+
+    @Column(name = "news_id")
+    private long newsId;
+
+    @Column(name = "update_at")
+    @UpdateTimestamp()
+    private LocalDateTime updatedAt;
+
+    @Column(name = "created_at")
+    @CreationTimestamp()
+    private LocalDateTime createdAt;
+
+    private boolean deleted = false;
+
+}
