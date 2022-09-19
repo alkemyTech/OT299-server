00bfa2768f7ff09f698953e58b0e0c4079541d9f
NicolasGastonVarela <101136113+NicolasGastonVarela@users.noreply.github.com>
2022-09-19 16:04:08 -0300

    @Builder implemented when possible (in model and dto)

--- src/main/java/com/alkemy/ong/data/entities/CategoriesEntity.java
+++ src/main/java/com/alkemy/ong/data/entities/CategoriesEntity.java
@@ -1,39 +1,36 @@
 package com.alkemy.ong.data.entities;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.SQLDelete;
 import org.hibernate.annotations.UpdateTimestamp;
 import org.hibernate.annotations.Where;
-
 import javax.persistence.*;
 import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "categories")
-@SQLDelete(sql = "UPDATE categories SET delete=true WHERE id=?")
-@Where(clause = "false")
+@SQLDelete(sql = "UPDATE categories SET deleted=true WHERE id=?")
+@Where(clause = "deleted = false")
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
+@Builder
 public class CategoriesEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
-    @Column(nullable = false)
     private String name;
     private String description;
     private String image;
-    @Column(name = "updated_at")
-    @UpdateTimestamp
-    private LocalDateTime updatedAt;
     @Column(name = "created_at")
     @CreationTimestamp
     private LocalDateTime createdAt;
-    private boolean delete = false;
+    @Column(name = "updated_at")
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
+    @Column(name = "deleted")
+    private boolean deleted = false;
 }
