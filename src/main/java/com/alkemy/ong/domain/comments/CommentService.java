2907a449ff5404d563d02b784463a9dcb7a24d4b
angelamonsalvep <54193886+angelamonsalvep@users.noreply.github.com>
2022-09-20 15:41:23 -0500

    OT299-76 Endpoint that returns the body of the comments ordered by date of creation. (#23)

---
+++ src/main/java/com/alkemy/ong/domain/comments/CommentService.java
@@ -0,0 +1,16 @@
+package com.alkemy.ong.domain.comments;
+
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+import java.util.List;
+
+@AllArgsConstructor
+@Service
+public class CommentService {
+
+    CommentGateway commentGateway;
+
+    public List<Comment> findAll(){
+        return commentGateway.findAll();
+    }
+}
