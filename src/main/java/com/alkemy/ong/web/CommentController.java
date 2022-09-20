741efaf645f5070547a73684b55fde2423a872da
Angela Monsalve <angelapmonsalve@gmail.com>
2022-09-20 18:46:21 -0500

    OT299-79 Endpoint development that allows you to delete a comment by its id.

--- src/main/java/com/alkemy/ong/web/CommentController.java
+++ src/main/java/com/alkemy/ong/web/CommentController.java
@@ -6,20 +6,25 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
 @AllArgsConstructor
 @RestController
+@RequestMapping("/comments")
 public class CommentController {
     private final CommentService commentService;
 
-    @GetMapping("/comments")
+    @GetMapping()
     public List<CommentDto> findAll(){
         return commentService.findAll().stream().map(this::commentToDto).collect(Collectors.toList());
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteComment(@PathVariable Long id) {
+        commentService.deleteById(id);
     }
 
     private CommentDto commentToDto(Comment comment){
