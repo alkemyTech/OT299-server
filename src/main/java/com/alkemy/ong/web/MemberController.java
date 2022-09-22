677ae92632c389392156ca3f911fadce173a4643
Ginette Etchichury <97034163+etchichuryg@users.noreply.github.com>
2022-09-22 22:33:40 +0200

    Ot299 70 (#33)
    * add files
    
    * Endpoint Member create()
    
    * Endpoint Member create()
    
    * OT299-66 added Create testimonials endpoint (#32)
    
    * OT299-66 added Create testimonials endpoint
    
    * OT299-66 added Create testimonials endpoint [Updated]
    
    * add files
    
    * Endpoint Member create()
    
    * Endpoint Member create()
    
    Co-authored-by: Oriol <72409843+Dev-OriolCC@users.noreply.github.com>
    Co-authored-by: lucas.paoliello <paoliellolucas@gmail.com>

--- src/main/java/com/alkemy/ong/web/MemberController.java
+++ src/main/java/com/alkemy/ong/web/MemberController.java
@@ -3,9 +3,12 @@ package com.alkemy.ong.web;
 import com.alkemy.ong.domain.members.Member;
 import com.alkemy.ong.domain.members.MemberService;
 import lombok.*;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
+import javax.validation.constraints.Pattern;
 import java.time.LocalDateTime;
 import java.util.List;
 
@@ -26,10 +29,18 @@ public class MemberController {
     }
 
     @DeleteMapping(path = "/{id}")
-    public ResponseEntity delete(@PathVariable(value = "id") Long id){
+    public ResponseEntity delete(@PathVariable Long id){
         memberService.deleteById(id);
         return ResponseEntity.noContent().build();
     }
+
+    @PostMapping()
+    public ResponseEntity<MemberDto> create(@Valid @RequestBody MemberDto memberDto){
+        Member member = memberService.create(toModel(memberDto));
+        return new ResponseEntity<>(toDto(member),HttpStatus.CREATED);
+
+    }
+
     private MemberDto toDto (Member member){
         return  MemberDto.builder()
                 .id(member.getId())
@@ -43,6 +54,20 @@ public class MemberController {
                 .deleted(member.isDeleted())
                 .build();
     }
+
+    public Member toModel(MemberDto memberDto){
+        return Member.builder()
+                .id(memberDto.getId())
+                .name(memberDto.getName())
+                .facebookUrl(memberDto.getFacebookUrl())
+                .instagramUrl(memberDto.getInstagramUrl())
+                .linkedinUrl(memberDto.getLinkedinUrl())
+                .image(memberDto.getImage())
+                .createdAt(memberDto.getCreatedAt())
+                .updatedAt(memberDto.getUpdateAt())
+                .deleted(memberDto.isDeleted())
+                .build();
+    }
     @Setter
     @Getter
     @AllArgsConstructor
@@ -50,6 +75,7 @@ public class MemberController {
     @Builder
     public static class MemberDto {
         private Long id;
+        @Pattern(regexp="^[A-Za-z]*$",message = "Invalid Input")
         private String name;
         private String facebookUrl;
         private String instagramUrl;
