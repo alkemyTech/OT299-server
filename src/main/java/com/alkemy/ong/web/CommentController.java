6285a30adb80698566cd038fb13c50222b358268
Angela Monsalve <angelapmonsalve@gmail.com>
2022-09-23 16:46:32 -0500

    -The endpoint that allows a user to add a comment to a specific post is developed. -Validation is added to verify that the user enters the required fields (postId, userId, body).

--- src/main/java/com/alkemy/ong/web/CommentController.java
+++ src/main/java/com/alkemy/ong/web/CommentController.java
@@ -6,9 +6,13 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotEmpty;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -22,8 +26,14 @@ public class CommentController {
     public ResponseEntity<List<CommentDto>> findAll(){
         return ResponseEntity.ok()
                 .body(commentService.findAll().stream()
-                        .map(this::commentToDto)
+                        .map(this::toDto)
                         .collect(Collectors.toList()));
+    }
+
+    @PostMapping()
+    public ResponseEntity<CommentDto> createComment(@Valid @RequestBody CommentDto commentDto){
+        Comment comment = commentService.createComment(toModelForCreate(commentDto));
+        return new ResponseEntity<>(toDtoForCreate(comment), HttpStatus.CREATED);
     }
 
     @DeleteMapping("/{id}")
@@ -32,14 +42,39 @@ public class CommentController {
         return ResponseEntity.noContent().build();
     }
 
-    private CommentDto commentToDto(Comment comment){
-        return CommentDto.builder().body(comment.getBody()).build();
+    private CommentDto toDto(Comment comment){
+        return CommentDto.builder()
+                .body(comment.getBody()).build();
     }
+
+    private CommentDto toDtoForCreate(Comment comment){
+        return CommentDto.builder()
+                .postId(comment.getNewsId())
+                .userId(comment.getUserId())
+                .body(comment.getBody()).build();
+    }
+
+    private Comment toModelForCreate(CommentDto commentDto){
+
+        return Comment.builder()
+                .newsId(commentDto.getPostId())
+                .userId(commentDto.getUserId())
+                .body(commentDto.getBody()).build();
+    }
+
 
     @Getter
     @Setter
     @Builder
     public static class CommentDto{
+
+        @Min(value = 1, message = "The field postId cannot be null or less than 1.")
+        private long postId;
+
+        @Min(value = 1, message = "The field userId cannot be null or less than 1.")
+        private long userId;
+
+        @NotEmpty(message = "The field body cannot be null or empty.")
         private String body;
     }
 }
