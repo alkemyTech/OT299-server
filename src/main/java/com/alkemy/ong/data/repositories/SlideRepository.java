e0c29cddf58aac8bf60b4e0b0aa0f548f3c620ba
AleIHC <105821230+AleIHC@users.noreply.github.com>
2022-09-23 20:56:35 -0300

    OT299-57 (#37)
    * Slides entity and repository created
    
    * New fields added to slides entity
    
    * New columns to slides table added
    
    * Conflicts with schema.sql
    
    * More conflicts with schema.sql solved
    
    * More conflicts with schema.sql solved pt2
    
    * More conflicts with schema.sql solved pt3
    
    * Slides GET endpoint
    
    * Slides GET endpoint, changes applied
    
    * Slides GET endpoint, schema changes applied
    
    * Changes to SlideService
    
    * Slides elimination endpoint
    
    * Slides elimination endpoint changes
    
    * Slides elimination endpoint changes to data
    
    * Changes to Slides elimination endpoint
    
    * Slide details by ID endpoint
    
    * Changes to data

--- src/main/java/com/alkemy/ong/data/repositories/SlideRepository.java
+++ src/main/java/com/alkemy/ong/data/repositories/SlideRepository.java
@@ -5,12 +5,12 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface SlideRepository extends CrudRepository<SlideEntity, Long> {
 
     List<SlideEntity> findAll();
-
 
     void deleteById(Long id);
 }
