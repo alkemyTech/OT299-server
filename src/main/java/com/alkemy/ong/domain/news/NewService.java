c7c7c635b5add17a567eddd795abaa891c0c6632
etchichuryg <ginettetchichury@gmail.com>
2022-09-23 10:08:03 -0300

    Endpoint New findById()

--- src/main/java/com/alkemy/ong/domain/news/NewService.java
+++ src/main/java/com/alkemy/ong/domain/news/NewService.java
@@ -3,6 +3,8 @@ package com.alkemy.ong.domain.news;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 @AllArgsConstructor
 public class NewService {
@@ -10,6 +12,16 @@ public class NewService {
     private final NewGateway newGateway;
 
     public void deleteById (Long id){
+
         newGateway.deleteById(id);
+    }
+
+    public List<New> findAll(){
+        return newGateway.findAll();
+    }
+
+    public New findById( Long id){
+
+        return newGateway.findById(id);
     }
 }
